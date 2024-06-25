988876e277f21f2832602704774fb8d46a5f13e3
zaidy-mughal <zaidamjadse@gmail.com>
2024-06-25 23:53:59 +0500

    completed code

--- InputOutput.py
+++ InputOutput.py
@@ -1,1 +1,14 @@
-# this function is used to read the txt file and count the frequency of 
+# this function is used to read the txt file and count the frequency of each word
+
+def fileReader(filename):
+    with open(filename,'r',encoding='utf-8') as file:
+        return file.read()
+
+
+filename = 'test.txt'
+with open(filename,'w',encoding='utf-8') as file2:
+    file2.write('My name is zaid amjad')
+
+content = fileReader(filename)
+print(content)
+    
